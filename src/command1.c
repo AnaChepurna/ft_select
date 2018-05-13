596f89525e862d02eb421566ea689c869315bf36
Anastasia CHEPURNA <achepurn@e2r3p9.unit.ua>
2018-05-13 13:19:18 +0300

    arrows added

--- src/command1.c
+++ src/command1.c
@@ -3,16 +3,30 @@
 int		command_carriage(t_interface *in, int mode)
 {
 	if (mode == DOWN)
+	{
 		in->carriage++;
+		if (in->carriage == in->len)
+			in->carriage = 0;
+	}
 	else if (mode == UP)
-		in->carriage++;
+	{
+		in->carriage--;
+		if (in->carriage == -1)
+			in->carriage = in->len - 1;
+	}
 	else if (mode == BACK)
+	{
+		if (in->carriage < in->lines)
+			while (in->carriage < in->len)
+				in->carriage += in->lines;
 		in->carriage -= in->lines;
+	}
 	else if (mode == FORWARD)
+	{
 		in->carriage += in->lines;
-	if (in->carriage >= in->len)
-		in->carriage -= in->lines;
-	if (in->carriage < 0)
-		in->carriage = in->len - in->carriage;
+		if (in->carriage >= in->len)
+			while (in->carriage >= in->lines)
+				in->carriage -= in->lines;
+	}
 	return (1);
 }
--- src/control.c
+++ src/control.c
@@ -2,79 +2,14 @@
 
 static int		parse_arrows(char *key, t_interface *in)
 {
-	char *str;
-	int i;
-
-	i = 0;
-	if (ft_strequ(key, (str = tgetstr("do", NULL))))
-	{
-		printf("\n");
-		i = 0;
-		while (i++ < 4)
-			ft_putnbr(*str++);
-		printf("down\n");
+	if (ft_strequ(key, "[B"))
 		return (command_carriage(in, DOWN));
-	}
-	else
-	{
-		i = 0;
-		printf("not down -> \n");
-		while (i++ < 4)
-			ft_putnbr(*str++);
-	}
-	if ((ft_strequ(key, (str =tgetstr("up", NULL)))))
-	{
-		printf("\n");
-		i = 0;
-		while (i++ < 4)
-			ft_putnbr(*str++);
-		printf("up\n");
+	if ((ft_strequ(key, "[A")))
 		return (command_carriage(in, UP));
-	}
-	else
-	{
-		i = 0;
-		printf("not up -> \n");
-		while (i++ < 4)
-			ft_putnbr(*str++);
-	}
-	if ((ft_strequ(key, (str =tgetstr("nd", NULL)))))
-	{
-		printf("\n");
-		i = 0;
-		while (i++ < 4)
-			ft_putnbr(*str++);
-		printf("forward\n");
+	if ((ft_strequ(key, "[C")))
 		return (command_carriage(in, FORWARD));
-	}
-	else
-	{
-		i = 0;
-		printf("not forward -> \n");
-		while (i++ < 4)
-			ft_putnbr(*str++);
-	}
-	if ((ft_strequ(key, (str =tgetstr("le", NULL)))))
-	{
-		printf("\n");
-		i = 0;
-		while (i++ < 4)
-			ft_putnbr(*str++);
-		printf("back\n");
+	if ((ft_strequ(key, "[D")))
 		return (command_carriage(in, BACK));
-	}
-	else
-	{
-		i = 0;
-		printf("not back -> \n");
-		while (i++ < 4)
-			ft_putnbr(*str++);
-	}
-	i = 0;
-	printf("this is -> \n");
-		while (i++ < 4)
-			ft_putnbr(*key++);
-	printf("nope\n");
 	return (0);
 }
 
@@ -93,7 +28,7 @@ void			parse_controls(t_interface *in)
 		ft_memset(key, '\0', 5);
 		read(0, &key, 4);
 		parse_arrows(key, in);
-		//manage_interface();
+		manage_interface();
 	}
 
 }
